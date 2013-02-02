abf219188b2d1bbb14d6fb860797f6fc34a27e7c
Shajin <shajinvk@gmail.com>
2013-02-02 23:52:19 +0530

    Layout editing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+  get '/help' , to: 'static_pages#help'
 
-  get "static_pages/about"
+  get '/about' , to: 'static_pages#about'
   
-  get 'static_pages/contact'
+  get '/contact', to: 'static_pages#contact'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -55,7 +54,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
